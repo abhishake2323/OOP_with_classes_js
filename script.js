fd1086d8b0a2de096eb88791aac8393434e18c88
abhishek <abhishakem@hotmail.com>
2024-12-13 17:01:05 +0530

    Added second child class and inherited it

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ class ProductProperties {
     }
 
 }
+
+
+class PerishableProductProperties extends ProductProperties{
+    constructor(name, price, quantity,expirationDate ) {
+        super(name,price,quantity)
+       this.expirationDate =expirationDate ;
+
+    }
+
+    toString(){
+        return `Product: ${this.name}, Price: $${this.price}, Quantity: ${this.quantity},Expiration Date: ${this.expirationDate}"`
+    }
+
+}
